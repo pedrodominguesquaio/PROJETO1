1ce49f33fb2dbea3771807c3f78829c8b7d86020
pedrodominguesquaio <00001126901957sp@al.educacao.sp.gov.br>
2024-07-31 14:53:25 -0300

    Create Sketch.js

---
+++ Sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(1000, 800);
+background("blue");
+  
+}
+
+function draw() {
+  
+  
+  fill("green")
+ stroke("white")
+  
+       if (mouseIsPressed) {
+  circle(mouseX,mouseY,100,100)
+
+       }
+}
